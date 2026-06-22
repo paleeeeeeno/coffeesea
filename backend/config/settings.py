a1fd081035c7a0bd929b3306d5ecceda49f85a7b
paleeeeeeno <paleeeeeeeno@gmail.com>
2026-06-22 13:55:49 +1000

    Update backend settings for Render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -109,7 +109,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-   "http://localhost:5173",
+    "http://localhost:5173",
     "https://coffeesea.onrender.com",
     "https://*.onrender.com",
 ]
