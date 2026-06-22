ff38613d3ad42b3101d53c8a9c63f5ddec66f574
paleeeeeeno <paleeeeeeeno@gmail.com>
2026-06-22 14:56:23 +1000

    Fix order items saving

--- backend/orders/serializers.py
+++ backend/orders/serializers.py
@@ -5,16 +5,56 @@ from .models import Order, OrderItem, BonusAccount
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
-        fields = "__all__"
+        fields = [
+            "id",
+            "product",
+            "size",
+            "modifiers",
+            "quantity",
+            "final_price",
+        ]
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    items = OrderItemSerializer(many=True, read_only=True)
+    items = OrderItemSerializer(many=True)
 
     class Meta:
         model = Order
-        fields = "__all__"
-        read_only_fields = ["user", "bonus_earned"]
+        fields = [
+            "id",
+            "user",
+            "delivery_type",
+            "address",
+            "cafe_address",
+            "status",
+            "total_price",
+            "bonus_earned",
+            "created_at",
+            "items",
+        ]
+        read_only_fields = [
+            "user",
+            "status",
+            "bonus_earned",
+            "created_at",
+        ]
+
+    def create(self, validated_data):
+        items_data = validated_data.pop("items", [])
+
+        order = Order.objects.create(**validated_data)
+
+        for item_data in items_data:
+            modifiers = item_data.pop("modifiers", [])
+
+            order_item = OrderItem.objects.create(
+                order=order,
+                **item_data
+            )
+
+            order_item.modifiers.set(modifiers)
+
+        return order
 
 
 class BonusAccountSerializer(serializers.ModelSerializer):
